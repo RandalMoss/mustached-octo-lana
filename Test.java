4adcc7b5e28c5916f2a231811d040f1671bbdba4
Randal Moss <randy@randy-linux.(none)>
2014-04-21 13:55:41 -0700

    modify 2

--- Test.java
+++ Test.java
@@ -1,7 +1,8 @@
 public class Test{
 	public static void main(String[] args){
 
-		System.out.println("Hello World");
+		System.out.println("Peanut Butter");
+		System.out.println("Jelly Time");
 	}	
 
 }
